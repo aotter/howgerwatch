540f69641d2726b3171591ca8d84ea322e6c0ce8
guyspy <guyspy@gmail.com>
2021-03-05 01:00:23 +0800

    add healthcheck

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/img/:url', function (req, res) {
     request(req.params.url).pipe(res);
 })
 
+app.get('/healthcheck', function (req, res) {
+    res.sendStatus(200);
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
